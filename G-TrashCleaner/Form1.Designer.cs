03902fe28bde811a1fb443d0e9ff66fe46507e07
Administrador <Administrador@TECHSUPPORT-BIC>
2025-06-02 18:53:26 -0600

    Add project files.

---
+++ G-TrashCleaner/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace G_TrashCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnSelAll = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.ColumnWidth = 1;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormatString = "File{0}";
+            this.checkedListBox1.HorizontalExtent = 1;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 36);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(787, 344);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClean.Location = new System.Drawing.Point(627, 7);
+            this.btnClean.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(172, 53);
+            this.btnClean.TabIndex = 1;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnScan.Location = new System.Drawing.Point(454, 7);
+            this.btnScan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(172, 53);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnSelAll
+            // 
+            this.btnSelAll.Location = new System.Drawing.Point(7, 7);
+            this.btnSelAll.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSelAll.Name = "btnSelAll";
+            this.btnSelAll.Size = new System.Drawing.Size(153, 53);
+            this.btnSelAll.TabIndex = 2;
+            this.btnSelAll.Text = "Select All";
+            this.btnSelAll.UseVisualStyleBackColor = true;
+            this.btnSelAll.Click += new System.EventHandler(this.btnSelAll_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSelAll);
+            this.panel1.Controls.Add(this.btnClean);
+            this.panel1.Controls.Add(this.btnScan);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(12, 406);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(811, 64);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(12, 13, 12, 13);
+            this.groupBox1.Size = new System.Drawing.Size(811, 393);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "WipeOut";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 483);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(12, 13, 12, 13);
+            this.Text = "G-WipeOut";
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnSelAll;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ G-TrashCleaner/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G_TrashCleaner
+{
+    public partial class Form1 : Form
+    {
+        // DWM API
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        public static extern void DwmEnableComposition(bool enable);
+
+        // Get all files in the temp directory
+        private string[] files = new[]
+        {
+            Path.GetTempPath(),                                                     // User Temp Folder
+            @"C:\Windows\Temp",                                                     // Windows Temp Folder
+            @"C:\Windows\SoftwareDistribution\Download",                            // Windows Update Cache
+            @"C:\Windows\Prefetch",                                                 // Windows Prefetch
+            @"C:\Users\Public\Documents\Microsoft Shared\Windows Live",             // Windows Live files
+            @"C:\ProgramData\Microsoft\Windows\WER",                                // Windows Error Reporting
+            @"C:\Users\<YourUsername>\AppData\Local\Microsoft\Windows\INetCache",   // Internet Cache
+       
+            // Application temp/cache
+            @"C:\Users\<YourUsername>\AppData\Local\Temp",                          // User-specific Temp
+            @"C:\Users\<YourUsername>\AppData\Local\Microsoft\Windows\Temporary Internet Files", // Old IE cache
+            @"C:\Users\<YourUsername>\AppData\Local\Microsoft\Windows\WebCache",    // Edge/IE WebCache
+            @"C:\Users\<YourUsername>\AppData\Local\Microsoft\Windows\Explorer",    // Explorer thumbnail cache
+            @"C:\Users\<YourUsername>\AppData\Local\CrashDumps",                    // Application crash dumps
+            @"C:\Users\<YourUsername>\AppData\Local\Packages",                      // UWP app caches (subfolders)
+            @"C:\Users\<YourUsername>\AppData\Roaming\Microsoft\Windows\Recent",    // Recent files
+            @"C:\Users\<YourUsername>\AppData\Roaming\Microsoft\Windows\Cookies",   // Cookies
+            @"C:\Users\<YourUsername>\AppData\Local\Google\Chrome\User Data\Default\Cache", // Chrome cache
+            @"C:\Users\<YourUsername>\AppData\Local\Mozilla\Firefox\Profiles",      // Firefox profiles (cache inside)
+            @"C:\$Recycle.Bin",                                                     // Recycle Bin (system-wide)
+            @"C:\Windows\Logs",                                                     // Windows logs
+            @"C:\Windows\System32\LogFiles",                                        // System log files
+            @"C:\Windows\System32\spool\PRINTERS",                                  // Print spooler files
+            @"C:\Windows\Debug",                                                    // Debug logs
+            @"C:\Windows\Minidump"                                                  // Crash dumps
+        };
+        public Form1()
+        { 
+            InitializeComponent();
+            // Enable DWM Composition
+            DwmEnableComposition(true);
+        }
+
+        public void Scantmp()
+        {
+            // Get the path to the temp directory
+            string tempPath = Path.GetTempPath();
+
+            // Check if the path is not null or empty
+            if (!string.IsNullOrEmpty(tempPath))
+            {
+                try
+                {  // Get all files in the temp directory
+                    files = Directory.GetFiles(tempPath); 
+                    checkedListBox1.Items.Clear();  
+                    foreach (string file in files)
+                    {
+                       checkedListBox1.Items.Add(file);
+                    } 
+
+                    foreach(string files in Directory.GetFiles(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Prefetch")))
+                    {
+                        checkedListBox1.Items.Add(files);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Access to the Prefetch directory is denied. Please run the application as administrator to access it.");
+                } 
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An Error occurred: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Temp path could not be determined.");
+            }
+
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete the selected files?", "Confirm Deletion", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // Collect items to remove
+                List<string> filesToRemove = new List<string>();
+
+                foreach (string fl in checkedListBox1.CheckedItems)
+                {
+                    try
+                    {
+                        File.Delete(fl);
+                        filesToRemove.Add(fl); // Collect the file for removal
+                    }
+                    catch (Exception ex)
+                    {
+                       Console.WriteLine($"Failed to delete: {fl}; Error Message: {ex.Message}", "G-WipeOut Fail to Delete");
+                    }
+                }
+
+                // Remove files from CheckedListBox after iteration
+                foreach (string fl in filesToRemove)
+                {
+                    checkedListBox1.Items.Remove(fl);
+                }
+
+                // Optionally, refresh the list after deletion
+                Scantmp();
+            }
+
+            //string tmpfile = "";
+            //try
+            //{
+            //    foreach (string fl in files)
+            //    {
+            //        tmpfile = fl;
+            //        File.Delete(fl);
+            //    }
+            //}
+            //catch( Exception ex)
+            //{ Console.WriteLine($" fail to delete : {tmpfile}; \nError Message :{ex.Message}" ); }
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            Scantmp();
+        }
+
+        private void btnSelAll_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true); // Select all items
+            }
+        }
+    }
+}
